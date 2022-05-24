d3c918c14969d9752dce038c4d57f2e1ecd3b9f1
IvanChudiyovuch <ivanchudiyovuch@ukr.net>
2022-05-24 22:58:34 +0300

    Update task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const makeListIngredients = document.querySelector('#ingredients');
+
+const elements = ingredients.map(element => {
+const liEl = document.createElement('li');
+liEl.classList.add('item');
+liEl.textContent = ingredients[element];
+
+  return liEl;
+});
+
+console.log(elements);
+makeListIngredients.append(...elements);
